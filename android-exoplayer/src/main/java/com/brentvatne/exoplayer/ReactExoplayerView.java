7a3dd83ea7f63f584aa58face9448e1a34c0e598
Zhulduz Rakova <zhankenova@gmail.com>
2019-10-16 18:06:04 +0600

    Add setKeepScreenOn to Expo player

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.accessibility.CaptioningManager;
 import android.widget.FrameLayout;
+import android.view.WindowManager;
 
 import com.brentvatne.react.R;
 import com.brentvatne.receiver.AudioBecomingNoisyReceiver;
@@ -224,11 +225,13 @@ class ReactExoplayerView extends FrameLayout implements
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         initializePlayer();
+        setKeepScreenOn(true);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        setKeepScreenOn(false);
         /* We want to be able to continue playing audio when switching tabs.
          * Leave this here in case it causes issues.
          */
@@ -570,9 +573,6 @@ class ReactExoplayerView extends FrameLayout implements
         } else {
             initializePlayer();
         }
-        if (!disableFocus) {
-            setKeepScreenOn(true);
-        }
     }
 
     private void pausePlayback() {
@@ -581,7 +581,6 @@ class ReactExoplayerView extends FrameLayout implements
                 setPlayWhenReady(false);
             }
         }
-        setKeepScreenOn(false);
     }
 
     private void stopPlayback() {
@@ -1148,6 +1147,7 @@ class ReactExoplayerView extends FrameLayout implements
             } else {
                 pausePlayback();
             }
+            setKeepScreenOn(!isPaused);
         }
     }
 
